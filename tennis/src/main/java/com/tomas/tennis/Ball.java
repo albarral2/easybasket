6ed61921778436798f627bef64ea90d006ba7c21
albarral2 <jluis.albarral@gmail.com>
2021-11-22 02:16:53 +0100

    tennis: sound added

--- tennis/src/main/java/com/tomas/tennis/Ball.java
+++ tennis/src/main/java/com/tomas/tennis/Ball.java
@@ -5,54 +5,52 @@ import java.awt.Rectangle;
 
 public class Ball {
 
-  final static int DIAMETER = 30;
-  final static int SPEED = 2;
-  int x, y;
-  int vx, vy;
-  Game game;
+    final static int DIAMETER = 30;
+    final static int SPEED = 3;
+    int x, y;
+    int vx, vy;
+    Game game;
 
-  public Ball(Game game) {
-    x = 0;
-    y = 0;
-    vx = SPEED;
-    vy = SPEED;
-    this.game = game;
-  }
-
-  public void move() {
-    int xlimit = game.getWidth() - DIAMETER;
-    int ylimit = game.getHeight() - DIAMETER;
-
-    if (x + vx < 0) {
-      vx = SPEED;
-    } else if (x + vx > xlimit) {
-      vx = -SPEED;
+    public Ball(Game game) {
+        x = 0;
+        y = 0;
+        vx = SPEED;
+        vy = SPEED;
+        this.game = game;
     }
 
-    if (y + vy < 0) {
-      vy = SPEED;
-    } else if (y + vy > ylimit) {
-      //vy = -SPEED;
-      game.gameOver();
+    public void move() {
+        int xlimit = game.getWidth() - DIAMETER;
+        int ylimit = game.getHeight() - DIAMETER;
+
+        x = x + vx;
+        y = y + vy;
+
+        if ((x < 0) || (x > xlimit)) {
+            Sound.BALL.play();
+            vx = -vx;
+        }
+
+        if ((y < 0) || (y > ylimit)) {
+            game.gameOver();
+        }
+
+        // if ball colides racket it goes up again
+        if (collision()) {
+            Sound.BALL.play();
+            vy = -vy;
+        }
     }
 
-    // if ball colides racket it goes up again
-    if (collision()) {
-      vy = -SPEED;
+    public void paint(Graphics2D g) {
+        g.fillOval(x, y, DIAMETER, DIAMETER);
     }
-    x = x + vx;
-    y = y + vy;
-  }
 
-  public void paint(Graphics2D g) {
-    g.fillOval(x, y, DIAMETER, DIAMETER);
-  }
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, DIAMETER, DIAMETER);
+    }
 
-  public Rectangle getBounds() {
-    return new Rectangle(x, y, DIAMETER, DIAMETER);
-  }
-
-  private boolean collision() {
-    return game.racket.getBounds().intersects(getBounds());
-  }
+    private boolean collision() {
+        return game.racket.getBounds().intersects(getBounds());
+    }
 }
--- tennis/src/main/java/com/tomas/tennis/Game.java
+++ tennis/src/main/java/com/tomas/tennis/Game.java
@@ -11,50 +11,59 @@ import javax.swing.JPanel;
 //@SuppressWarnings("serial")
 public class Game extends JPanel {
 
-  final static int WIDTH = 600;
-  final static int HEIGHT = 800;
-  Ball ball;
-  Racket racket;
+    final static int WIDTH = 400;
+    final static int HEIGHT = 400;
+    Ball ball;
+    Racket racket;
 
-  public Game() {
-    ball = new Ball(this);
-    racket = new Racket(this);
-    KeyListener listener = new Keyboard(this);
-    addKeyListener(listener);
-    setFocusable(true);
-  }
-
-  public static void main(String[] args) throws InterruptedException {
-    JFrame frame = new JFrame("Mini Tennis");
-    Game game = new Game();
-    frame.add(game);
-    frame.setSize(WIDTH, HEIGHT);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    while (true) {
-      game.move();
-      game.repaint();
-      Thread.sleep(10);
+    @SuppressWarnings({"static-access", "OverridableMethodCallInConstructor"})
+    public Game() {
+        ball = new Ball(this);
+        racket = new Racket(this);
+        Sound.BACK.loop();
+        KeyListener listener = new Keyboard(this);
+        addKeyListener(listener);
+        setFocusable(true);
     }
-  }
 
-  private void move() {
-    ball.move();
-    racket.move();
-  }
+    public static void main(String[] args) throws InterruptedException {
 
-  @Override
-  public void paint(Graphics g) {
-    super.paint(g);
-    Graphics2D g2d = (Graphics2D) g;
-    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    ball.paint(g2d);
-    racket.paint(g2d);
-  }
+        try {
+            JFrame frame = new JFrame("Mini Tennis");
+            Game game = new Game();
+            frame.add(game);
+            frame.setSize(WIDTH, HEIGHT);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-  public void gameOver() {
-    JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
-    System.exit(0);
-  }
+            while (true) {
+                game.move();
+                game.repaint();
+                Thread.sleep(10);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    private void move() {
+        ball.move();
+        racket.move();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        ball.paint(g2d);
+        racket.paint(g2d);
+    }
+
+    public void gameOver() {
+        Sound.BACK.stop();
+        Sound.GAMEOVER.play();
+        JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+        System.exit(0);
+    }
 }
